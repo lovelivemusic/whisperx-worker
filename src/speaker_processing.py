261cfa9b45861da4d8ecec5564ea5dad43aa2d7b
Gene Myers <gene@wyldnetworks.com>
2026-04-06 10:41:07 +0100

    Fix: pass device as string to SpeechBrain ECAPA encoder

--- src/speaker_processing.py
+++ src/speaker_processing.py
@@ -47,7 +47,7 @@ def _get_ecapa():
             from speechbrain.pretrained import EncoderClassifier
         _ECAPA = EncoderClassifier.from_hparams(
             source="speechbrain/spkrec-ecapa-voxceleb",
-            run_opts={"device": device},
+            run_opts={"device": str(device)},
         )
     return _ECAPA
 
